080a606b4daafb19ba8ddf092d0a30a9e689b221
Mateus <112036741+MateusTegue@users.noreply.github.com>
2025-07-22 18:13:13 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 const corsOptions = {
-    // origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173',
     origin: 'https://portfolio-frontend-react-rho.vercel.app', 
     credentials: true,
 }
